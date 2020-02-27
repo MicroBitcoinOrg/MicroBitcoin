8152a580c7bd5574eb2b549792ef5599ac1a7d0f
itwysgsl <iamstenman@protonmail.com>
2020-02-27 15:53:47 +0900

    net: Switch nPrevNodeCount to vNodesSize

--- src/net.cpp
+++ src/net.cpp
@@ -1226,7 +1226,7 @@ void CConnman::ThreadSocketHandler()
         if(vNodesSize != nPrevNodeCount) {
             nPrevNodeCount = vNodesSize;
             if(clientInterface)
-                clientInterface->NotifyNumConnectionsChanged(nPrevNodeCount);
+                clientInterface->NotifyNumConnectionsChanged(vNodesSize);
         }
 
         //
